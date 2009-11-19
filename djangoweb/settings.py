05849a6c4b14ce61eeccf4387e66f0c7f29fef7e
Alex Clemesha <clemesha@gmail.com>
2009-11-18 22:51:41 -0800

    Commented out the {% csrf_token %} template tags, as they are not default until Django 1.2, or can be installed custom if needed for an earlier version of Django.

--- djangoweb/settings.py
+++ djangoweb/settings.py
@@ -65,6 +65,7 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
+    #'django.middleware.csrf.CsrfResponseMiddleware',
 )
 
 ROOT_URLCONF = 'djangoweb.urls'
